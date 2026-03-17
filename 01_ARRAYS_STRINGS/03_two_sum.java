b0e89cfea43d41b3a3b430d3202886918dfbc85c
dharanikuppala <137318054+dharanikuppala@users.noreply.github.com>
2026-03-17 22:05:26 +0530

    Added hashtable soln for two_sum.java

--- 01_ARRAYS_STRINGS/03_two_sum.java
+++ 01_ARRAYS_STRINGS/03_two_sum.java
@@ -1,14 +1,32 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
-        for(int i = 0; i<nums.length;i++){
-            for( int j = i+1; j<nums.length;j++){
-                if(target == nums[i] + nums[j] ){
-                ans[0] = i;
-                ans[1] = j ;
+        if(nums.length <= 1){
+            return new int[]{};
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
             }
-            }
-            
-        } return ans;
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+        
     }
 }
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] ans = new int[2];
+//         for(int i = 0; i<nums.length;i++){
+//             for( int j = i+1; j<nums.length;j++){
+//                 if(target == nums[i] + nums[j] ){
+//                 ans[0] = i;
+//                 ans[1] = j ;
+//             }
+//             }
+            
+//         } return ans;
+//     }
+// }
